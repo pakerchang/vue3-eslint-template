bf18db79f9714e3ea5e91e76f18bde17d068c79b
pzdesu <pakerchang@gmail.com>
2023-03-06 16:45:29 +0800

    adjust project

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import eslintPlugin from 'vite-plugin-eslint';
-import { fileURLToPath, URL } from 'url';
+import eslintPlugin from "vite-plugin-eslint";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     eslintPlugin({
-      include: ['src/**/*.{js,vue,html}'],
+      include: ["src/**/*.{js,vue,html}"],
     }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
